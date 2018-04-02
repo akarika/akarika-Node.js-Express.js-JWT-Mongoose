d31e00253b7eaa9241bf56b7e4d9facd2216a973
akarika <roux.remy.m@gmail.com>
2018-04-02 20:13:17 +0200

    express-jwt

--- app.js
+++ app.js
@@ -1,91 +1,126 @@
-/**
- * IMPORT 
- */
 const express = require('express');
+const app = express();
+var favicon = require('serve-favicon');
 const bodyParser = require('body-parser');
 const multer = require('multer');
-
-
-
-const app = express();
 const upload = multer();
-let frenchMovies  = [];
-const PORT = 3000;
-/**
- * MIDDLEWARE
- */
+var path = require('path');
+
+const jwt = require('jsonwebtoken');
+// to verify token on the request header
+var expressJwt = require('express-jwt'); 
+
+const port = 3000;
+let frenchMovies = [];
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+const secret = 'qsdjS12ozehdoIJ123DJOZJLDSCqsdeffdg123ER56SDFZedhWXojqshduzaohduihqsDAqsdq';
+
+// to service static files from the public folder
 app.use('/public', express.static('public'));
-//app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
 
-/**
- * CONFIG
- */
-app.set('views', './views')
-app.set('view engine', 'ejs')
-app.get('/', ((req, res) => {
-    //res.send('Hells00!!!');
+// // favicon
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+// check token on all pages except the ones mentioned in unless()
+app.use(expressJwt({ secret: secret}).unless({ path: ['/', '/movies', '/movie-search', '/login']}));
+
+app.get('/', (req, res) => {
     res.render('index');
-}))
+});
 
-/*
-*REQUEST
-*/
-app.get('/moviesss', (req, res) => {
-    //res.send('bientot des films ici')
+app.get('/movies', (req, res) => {
+
     const title = "Films français des 30 dernières années";
-    frenchMovies = [{
-            title: 'Amélie poulin',
-            year: 2001
-        },
-        {
-            title: 'Buffet froid',
-            year: 1979
-        },
-        {
-            title: 'Diner de con',
-            year: 1998
-        },
-        {
-            title: 'De rouille et d\'os',
-            year: 1998
-        }
-    ];
-    res.render('movies', {movies : frenchMovies, title})
+    frenchMovies = [
+        { title: 'Le fabuleux destin d\'Amélie Poulain', year: 2001},
+        { title: 'Buffet froid', year: 1979},
+        { title: 'Le diner de cons', year: 1998},
+        { title: 'de rouille et d\'os', year: 2012}
+    ]
+    res.render('movies', { title: title, movies: frenchMovies});
 });
-const urlencodedParser = bodyParser.urlencoded({ extended: false })
-app.post('/movies',urlencodedParser ,(req,res)=>{
-    console.log('req.body.movietitle',req.body.movietitle);
-    console.log('YEAR',req.body.movieyear);
-    const newMovie = {title : req.body.movietitle, year : req.body.movietitle}
-    frenchMovies = [...frenchMovies,newMovie]
-    console.log(frenchMovies);
-    
-    res.sendStatus(201);
+
+//!\ In upload.fields([]), the empty array '[]' is required
+app.post('/movies', upload.fields([]), (req, res) => {
+    if (!req.body) {
+        return res.sendStatus(500);
+    } else {
+        const formData = req.body; 
+        console.log('form data: ', formData);
+        const newMovie = { title: formData.movietitle, year: formData.movieyear };
+        frenchMovies = [... frenchMovies, newMovie];
+        res.sendStatus(201);
+    } 
 });
-//app.get('/movies-details', (req,res)=>{
-//res.send('bientot des films ici')
-//});
-app.get('/movie-search', (req, res) =>{
+
+
+// create application/x-www-form-urlencoded parser
+// https://github.com/expressjs/body-parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.post('/movies-old-browser', urlencodedParser, (req, res) => {
+    if (!req.body) {
+        return res.sendStatus(500);
+    } else {    
+        frenchMovies = [... frenchMovies, { title: req.body.movietitle, year: req.body.movieyear }];
+        res.sendStatus(201);
+    } 
+});
+
+
+app.get('/movies/add', (req, res) => {
+    res.send('prochainement, un formulaire d\'ajout ici');
+});
+
+app.get('/movies/:id', (req, res) => {
+    const id = req.params.id;
+    res.render('movie-details');
+});
+
+app.get('/movie-search', (req, res) => {
     res.render('movie-search');
 });
-app.get('/movies/add', (req, res) => {
-    res.send('formulaire')
-});
-app.get('/movies/:id', (req, res) => {
-    //app.get('/movies/:id/:title', (req, res) => {
-    const id = req.params.id;
-    const title = req.params.title;
-    //  res.send(`film numéro ${id} et ${id2}`);
-    res.render('movies-details', {
-        movieId: id,
-        //        movieTitle:title
-    });
+
+app.get('/login', (req, res) => {
+    res.render('login', { title: 'Espace membre'});
 });
 
-/**
- * START SERV
- */
-app.listen(PORT, () => {
-    console.log(`listeninssssg on port ${PORT}`);
+const fakeUser = { email: 'testuser@testmail.fr', password: 'qsd' };
+
+app.post('/login', urlencodedParser, (req, res) => {
+    console.log('login post', req.body);
+    if (!req.body) {
+        return res.sendStatus(500);
+    } else {        
+        if(fakeUser.email === req.body.email && fakeUser.password === req.body.password) {
+            // iss means 'issuer'
+            const myToken = jwt.sign({iss: 'http://expressmovies.fr', user: 'Sam', role: 'moderator'}, secret);
+            console.log('myToken', myToken);
+            res.json(myToken);
+        } else {
+            res.sendStatus(401);
+        } 
+    } 
+});
+
+app.get('/member-only',(req, res) => {
+    console.log('req.user', req.user);
+    if(req.user.role === 'moderator') {
+        res.send(req.user);
+    };
+});
+
+// // to make the error message clearer
+// app.use(function (err, req, res, next) {
+//   if (err.name === 'UnauthorizedError') {
+//     res.status(401).send(err.inner);
+//   }
+// });
+
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
 });
